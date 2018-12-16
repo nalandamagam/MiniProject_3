ed70ad4af969fb746628c6a8d24f8cd242694a33
nalandamagam <nalandachowdary1993@gmail.com>
2018-12-15 19:14:25 -0500

    Added dashboard link to the welcome page and created feature tests for loginpage,registerpage and dashboard.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,6 +70,7 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
+                        <a href="{{ route('loginChart') }}">Dashboard</a>
                         <a href="{{ route('login') }}">Login</a>
 
                         @if (Route::has('register'))
